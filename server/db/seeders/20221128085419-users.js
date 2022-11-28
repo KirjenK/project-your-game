c3e034c72907f8b58e6db6bf7dcb43615263b615
Kirill <kirpitch@inbox.ru>
2022-11-28 12:28:23 +0300

    seeds

---
+++ server/db/seeders/20221128085419-users.js
@@ -0,0 +1,13 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        name: 'admin', email: 'admin@game.ru', password: '1234', createdAt: new Date(), updatedAt: new Date(),
+      }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
