1cb86b163dc7d5cc5004239853967413e77373be
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2022-11-28 15:29:28 +0200

    reg form, auth form, funck complete

--- client/src/components/Reg/Reg.jsx
+++ client/src/components/Reg/Reg.jsx
@@ -9,6 +9,9 @@ export default function Auth({ setUser }) {
     repeatPassword: '',
   });
 
+  const [answer, setAnswer] = useState(null);
+  const [formName, setFormName] = useState('regFormOne');
+
   const handleInput = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
@@ -24,31 +27,59 @@ export default function Auth({ setUser }) {
       body: JSON.stringify(form),
     })
       .then((res) => res.json())
-      .then((res) => setUser(res));
+      .then((res) => {
+        if (res.message === 'пароли не совпадают') {
+          setForm({
+            password: '',
+            repeatPassword: '',
+          });
+          setAnswer(res.message);
+        } else if (res.message === 'Пожалуйста заполните все поля') {
+          setAnswer(res.message);
+        } else if (res.message === 'пользователь уже существует') {
+          setForm({
+            name: '',
+          });
+          setAnswer(res.message);
+        } else if (res.message === 'пароль должен быть больше трех символов') {
+          setForm({
+            password: '',
+            repeatPassword: '',
+          });
+          setAnswer(res.message);
+        } else if (res.message === `${res.username}, поздравляем с успешной регистрацией !`) {
+          setUser({ username: res.username, userId: res.userId });
+          setAnswer(res.message);
+          setFormName('regFormTwo');
+        }
+      });
   };
 
   return (
     <div className="authForm">
-    <form onSubmit={handleSubmit}>
-    <div>
-    <label>Name</label>
-    <input type="text" value={form.name} name="name" onChange={handleInput} />
-    </div>
-    <div>
-    <label>Email address</label>
-    <input type="email" value={form.email} name="email" onChange={handleInput} />
-    </div>
-    <div>
-    <label>Password</label>
-    <input type="password" value={form.password} name="password" onChange={handleInput} />
-    </div>
-    <div>
-    <label>Repeat password</label>
-    <input type="password" value={form.repeatPassword} name="repeatPassword" onChange={handleInput} />
-    </div>
+      <form className={formName} onSubmit={handleSubmit}>
+      <div>
+      <label>Name</label>
+      <input type="text" value={form.name} name="name" onChange={handleInput} />
+      </div>
+      <div>
+      <label>Email address</label>
+      <input type="email" value={form.email} name="email" onChange={handleInput} />
+      </div>
+      <div>
+      <label>Password</label>
+      <input type="password" value={form.password} name="password" onChange={handleInput} />
+      </div>
+      <div>
+      <label>Repeat password</label>
+      <input type="password" value={form.repeatPassword} name="repeatPassword" onChange={handleInput} />
+      </div>
 
-    <button type="submit">Submit</button>
-    </form>
+      <button type="submit">Submit</button>
+      </form>
+      <div>
+        {answer}
+      </div>
     </div>
   );
 }
