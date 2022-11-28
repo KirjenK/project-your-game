8126c68cdb97fb0b1972f4e80ff671a054728865
Kirill <kirpitch@inbox.ru>
2022-11-28 11:00:48 +0300

    client init,react,eslint,rules

---
+++ client/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0,
+    'linebreak-style': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'react/prop-types': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+  },
+};
